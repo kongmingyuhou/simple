73be5ce151374a5562aa41c1fb44723c5927f10b
kongmingyuhou <yfyhc@outlook.com>
2019-11-03 21:36:38 +0800

    按钮控件提交

---
+++ app/src/main/java/com/example/simple/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.simple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_button_click.setOnClickListener { startActivity<ButtonClickActivity>() }
+    }
+}
